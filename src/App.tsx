655354ec2acdd873d913c00184e9d8220e96f393
Pedthep <pedthep@gmail.com>
2024-02-20 22:44:03 +0700

    remove test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Provider } from "react-redux";
 import store from "./stores/store";
 
-import Test from "./tests/test";
-import TestModal from "./tests/TestModal";
 import AppLayout from "./screens/_layout";
 
 const App = () => {
